48fcf2aaece7ef3f38a47bd7eadb03a2e41200d5
Noe19 <denissecumbal2015@hotmail.com>
2022-03-03 23:47:20 -0500

    Angular_consumo_de_apis

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { ClientelaravelService } from './service/clientelaravel.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  title = 'frontend';
+
+  constructor (public servc:ClientelaravelService)
+  {
+
+    this.servc.getWards().subscribe(r=>{
+
+      console.warn(r);
+
+      console.table(r[0]);
+
+    })
+  }
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { WardComponent } from './prision/ward/ward.component';
+
+import { LoginComponent } from './prision/login/login.component';
+
+
+
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WardComponent,
+    LoginComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
